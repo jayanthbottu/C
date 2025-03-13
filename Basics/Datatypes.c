bcbde2cb122fdbdff28e77b85fe340888ec3a10a
Jayanth Bottu <147061704+jayanthbottu@users.noreply.github.com>
2025-03-13 11:10:50 +0530

    Create Datatypes.c

---
+++ Basics/Datatypes.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+int a = 100;
+float b = 15.9;
+char c = 'J';
+double d = 12.5463;
+char ch[10] = "Jayanth";
+printf("Integer     : %d\n",a);
+printf("Float       : %f\n",b);
+printf("Character   : %c\n",c);
+printf("Double      : %d\n",d);
+printf("String      : %s\n",ch);
+return 0;
+}
