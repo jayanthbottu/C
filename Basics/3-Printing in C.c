5c85742e5202d123fabedb526b241ebac2756b94
Jayanth Bottu <147061704+jayanthbottu@users.noreply.github.com>
2025-03-13 11:16:54 +0530

    Printing Outputs in C

---
+++ Basics/3-Printing in C.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+printf("Hello This is from print() method\n");
+puts("Line from puts() method");
+printf("Printing with Arguments into Output : %s","'This is Argument'");
+return 0;
+}
