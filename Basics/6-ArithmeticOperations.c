1232081358a9d09cec92267584c9d0b44432c0a0
Jayanth Bottu <147061704+jayanthbottu@users.noreply.github.com>
2025-03-13 11:51:48 +0530

    Arithmetic Operations

---
+++ Basics/6-ArithmeticOperations.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+int a = 20;
+int b = 10;
+puts ("     ARITHMETIC OPERATORS    ");
+printf("Addition        : %d\n",(a+b));
+printf("Subtraction     : %d\n",(a-b));
+printf("Multiplication  : %d\n",(a*b));
+printf("Division        : %ld\n",(a/b));
+printf("Modulus         : %ld",(a%b));
+return 0;
+}
+
+
+// Output
+     ARITHMETIC OPERATORS    
+Addition        : 30
+Subtraction     : 10
+Multiplication  : 200
+Division        : 2
+Modulus         : 0
